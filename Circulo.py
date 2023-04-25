9a3bc2f2a2e8d773494be57d2644bc9554f78156
bapsy <125379090+bapsy11-12@users.noreply.github.com>
2023-04-25 16:53:30 +0200

    Add files via upload

---
+++ Circulo.py
@@ -0,0 +1,14 @@
+class Circulo():
+
+    def __init__(radio:float):
+        self._radio=radio:
+    pass
+   
+
+    def area(self):
+    pass    
+
+
+    def circunferencia(self):
+    pass         
+        
